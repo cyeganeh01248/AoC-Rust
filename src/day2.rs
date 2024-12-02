0d0a787944161e85e4aaf5698ccfead1dcb1e7db
Christian Yeganeh <christianyeganeh@gmail.com>
2024-12-02 13:43:42 -0500

    Broke logic into separate function

--- src/day2.rs
+++ src/day2.rs
@@ -12,26 +12,30 @@ fn parse(input: &str) -> Vec<Vec<i32>> {
     output
 }
 
+fn is_safe(report: &[i32]) -> bool {
+    let mut all_ascending = true;
+    let mut all_descending = true;
+    let mut all_within_1_3 = true;
+
+    for i in 1..report.len() {
+        if report[i] < report[i - 1] {
+            all_ascending = false;
+        }
+        if report[i] > report[i - 1] {
+            all_descending = false;
+        }
+        if (report[i] - report[i - 1]).abs() > 3 || (report[i] - report[i - 1]).abs() < 1 {
+            all_within_1_3 = false;
+        }
+    }
+    (all_ascending || all_descending) && all_within_1_3
+}
+
 #[aoc(day2, part1)]
 fn part1(input: &[Vec<i32>]) -> String {
     let mut count = 0;
     for report in input.iter() {
-        let mut all_ascending = true;
-        let mut all_descending = true;
-        let mut all_within_1_3 = true;
-
-        for i in 1..report.len() {
-            if report[i] < report[i - 1] {
-                all_ascending = false;
-            }
-            if report[i] > report[i - 1] {
-                all_descending = false;
-            }
-            if (report[i] - report[i - 1]).abs() > 3 || (report[i] - report[i - 1]).abs() < 1 {
-                all_within_1_3 = false;
-            }
-        }
-        if (all_ascending || all_descending) && all_within_1_3 {
+        if is_safe(report) {
             count += 1;
         }
     }
@@ -47,23 +51,7 @@ fn part2(input: &[Vec<i32>]) -> String {
             if skip != report.len() {
                 new_report.remove(skip);
             }
-            let mut all_ascending = true;
-            let mut all_descending = true;
-            let mut all_within_1_3 = true;
-            for i in 1..new_report.len() {
-                if new_report[i] < new_report[i - 1] {
-                    all_ascending = false;
-                }
-                if new_report[i] > new_report[i - 1] {
-                    all_descending = false;
-                }
-                if (new_report[i] - new_report[i - 1]).abs() > 3
-                    || (new_report[i] - new_report[i - 1]).abs() < 1
-                {
-                    all_within_1_3 = false;
-                }
-            }
-            if (all_ascending || all_descending) && all_within_1_3 {
+           if is_safe(&new_report) {
                 count += 1;
                 break;
             }
