5ce347c208ebb1f06fa0b81b2efdff7e96432852
Christian Yeganeh <christianyeganeh@gmail.com>
2024-12-04 15:38:06 -0500

    Made bench just bench

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -5,6 +5,5 @@ for file in $(ls src | grep day); do
     day=${file:3:1}
     echo -------------- AoC Day $day --------------
     cargo aoc input -d $day
-    cargo aoc -d $day
     cargo aoc bench -d $day
 done
