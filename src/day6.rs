1e3a09a8906e197f5426f00658705d4ed498b692
Christian Yeganeh <christianyeganeh@gmail.com>
2024-12-06 19:25:56 -0500

    Changed part 1 to use a more general code.

--- src/day6.rs
+++ src/day6.rs
@@ -1,36 +1,79 @@
+use std::collections::HashSet;
+
 use aoc_runner_derive::{aoc, aoc_generator};
+use fxhash::{FxHashSet, FxHasher};
 
 use crate::parsers::v_grid_no_whitespace;
 #[aoc_generator(day6)]
-fn parse(input: &str) -> Vec<Vec<char>> {
-    v_grid_no_whitespace(input)
-}
+fn parse(input: &str) -> (Vec<Vec<char>>, (isize, isize), u8) {
+    let mut guard_r = 0isize;
+    let mut guard_c = 0isize;
+    let mut dir = 0u8;
 
-#[aoc(day6, part1)]
-fn part1(grid: &Vec<Vec<char>>) -> usize {
-    let mut grid = grid.clone();
-    let mut count = 0;
-    let (mut guard_r, mut guard_c) = (0isize, 0isize);
-    let mut dir = 0;
-
-    'find_guard: for (r, row) in grid.iter().enumerate() {
+    let mut grid = v_grid_no_whitespace(input);
+    for (r, row) in grid.iter().enumerate() {
         for (c, cell) in row.iter().enumerate() {
-            if ['^', '>', 'v', '<'].contains(cell) {
-                guard_r = r as isize;
-                guard_c = c as isize;
-                dir = match cell {
-                    '^' => 0,
-                    '>' => 1,
-                    'v' => 2,
-                    '<' => 3,
-                    _ => unreachable!(),
-                };
-                break 'find_guard;
+            match cell {
+                '^' => {
+                    guard_r = r as isize;
+                    guard_c = c as isize;
+                    dir = 0;
+                }
+                '>' => {
+                    guard_r = r as isize;
+                    guard_c = c as isize;
+                    dir = 1;
+                }
+                'v' => {
+                    guard_r = r as isize;
+                    guard_c = c as isize;
+                    dir = 2;
+                }
+                '<' => {
+                    guard_r = r as isize;
+                    guard_c = c as isize;
+                    dir = 3;
+                }
+                _ => {}
             }
         }
     }
+    grid[guard_r as usize][guard_c as usize] = '.';
+    (grid, (guard_r, guard_c), dir)
+}
 
-    // grid[guard_r as usize][guard_c as usize] = '#';
+#[aoc(day6, part1)]
+fn part1((grid, (guard_r, guard_c), dir): &(Vec<Vec<char>>, (isize, isize), u8)) -> usize {
+    let grid = grid.clone();
+
+    let (raw_results, _) = traverse_grid(&grid, (*guard_r, *guard_c), *dir);
+
+    let mut unique_results = HashSet::new();
+    for (dir, r, c) in raw_results {
+        unique_results.insert((r, c));
+    }
+    unique_results.len()
+}
+
+#[aoc(day6, part2)]
+fn part2((grid, (guard_r, guard_c), dir): &(Vec<Vec<char>>, (isize, isize), u8)) -> usize {
+    todo!()
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum TraverseResult {
+    Edge,
+    Loop,
+}
+
+fn traverse_grid(
+    grid: &Vec<Vec<char>>,
+    (mut guard_r, mut guard_c): (isize, isize),
+    mut dir: u8,
+) -> (FxHashSet<(u8, isize, isize)>, TraverseResult) {
+    let mut visited = FxHashSet::default();
+    visited.reserve(grid.len() * grid[0].len());
+    visited.insert((dir, guard_r, guard_c));
     loop {
         let (dr, dc) = match dir {
             0 => (-1, 0),
@@ -44,41 +87,22 @@ fn part1(grid: &Vec<Vec<char>>) -> usize {
             || guard_c + dc < 0
             || guard_c + dc >= grid[0].len() as isize
         {
-            count += 1;
-            break;
-        };
-        if grid[(guard_r + dr) as usize][(guard_c + dc) as usize] == '#' {
-            dir = (dir + 1) % 4;
-            grid[guard_r as usize][guard_c as usize] = match dir {
-                0 => '^',
-                1 => '>',
-                2 => 'v',
-                3 => '<',
-                _ => unreachable!(),
-            };
+            return (visited, TraverseResult::Edge);
+        } else if visited.contains(&(dir, guard_r + dr, guard_c + dc)) {
+            return (visited, TraverseResult::Loop);
         } else {
-            grid[guard_r as usize][guard_c as usize] = 'X';
-            if grid[(guard_r + dr) as usize][(guard_c + dc) as usize] != 'X' {
-                count += 1;
+            if grid[(guard_r + dr) as usize][(guard_c + dc) as usize] == '#' {
+                dir = (dir + 1) % 4;
+            } else {
+                guard_r += dr;
+                guard_c += dc;
+                if visited.contains(&(dir, guard_r, guard_c)) {
+                    panic!("How did we get here?");
+                }
+                visited.insert((dir, guard_r, guard_c));
             }
-            guard_r += dr;
-            guard_c += dc;
-            grid[guard_r as usize][guard_c as usize] = match dir {
-                0 => '^',
-                1 => '>',
-                2 => 'v',
-                3 => '<',
-                _ => unreachable!(),
-            };
         }
     }
-
-    count
-}
-
-#[aoc(day6, part2)]
-fn part2(grid: &Vec<Vec<char>>) -> String {
-    todo!()
 }
 
 #[cfg(test)]
@@ -119,7 +143,7 @@ mod tests {
 #.........
 ......#..."
             )),
-            "<RESULT>"
+            6
         );
     }
 }
