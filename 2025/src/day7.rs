f4506de77fd04d4f42173ff0590b9bf12f80e39b
Chris Yeganeh <christianyeganeh@gmail.com>
2025-12-08 22:32:49 -0500

    Completed day 7 part 1

--- 2025/src/day7.rs
+++ 2025/src/day7.rs
@@ -1,31 +1,100 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use fxhash::{FxHashMap as HashMap, FxHashSet as HashSet};
+
 #[aoc_generator(day7)]
-fn parse(input: &str) -> String {
-    todo!()
+fn parse(input: &str) -> (usize, HashMap<usize, Vec<usize>>, usize) {
+    let lines = input.lines().collect::<Vec<_>>();
+    let mut out = HashMap::default();
+    for (row, line) in lines.iter().enumerate() {
+        for (col, char) in line.chars().enumerate() {
+            if char == '^' {
+                let v: &mut Vec<usize> = out.entry(row).or_default();
+                v.push(col);
+            }
+        }
+    }
+    (lines[0].find('S').unwrap(), out, lines.len())
 }
 
 #[aoc(day7, part1)]
-fn part1(input: &str) -> String {
-    todo!()
+fn part1((start, input, height): &(usize, HashMap<usize, Vec<usize>>, usize)) -> u64 {
+    let mut cols_to_check = HashSet::default();
+    cols_to_check.insert(*start);
+    let mut r = 0;
+    let mut count = 0;
+    while r < *height {
+        let row = input.get(&r);
+        if row.is_none() {
+            r += 1;
+            continue;
+        }
+        let row = row.unwrap();
+        let mut new_cols = cols_to_check.clone();
+        for col in cols_to_check.iter() {
+            if row.contains(col) {
+                println!("Colision at {r} {col}");
+                count += 1;
+                new_cols.remove(&col);
+                new_cols.insert(col + 1);
+                new_cols.insert(col - 1);
+            }
+        }
+        cols_to_check = new_cols;
+        r += 1;
+    }
+    count
 }
 
 #[aoc(day7, part2)]
-fn part2(input: &str) -> String {
+fn part2((start, input, height): &(usize, HashMap<usize, Vec<usize>>, usize)) -> String {
     todo!()
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
+
+    const EXAMPLE_INPUT: &str = indoc! {"
+        .......S.......
+        ...............
+        .......^.......
+        ...............
+        ......^.^......
+        ...............
+        .....^.^.^.....
+        ...............
+        ....^.^...^....
+        ...............
+        ...^.^...^.^...
+        ...............
+        ..^...^.....^..
+        ...............
+        .^.^.^.^.^...^.
+        ..............."};
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part1(&parse(EXAMPLE_INPUT)), 21);
+    }
+
+    #[test]
+    fn part1_example_2() {
+        assert_eq!(
+            part1(&parse(indoc! {"
+            ....S....
+            .........
+            ....^....
+            ...^.^...
+            .........
+            ........."
+            })),
+            3
+        );
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part2(&parse(EXAMPLE_INPUT)), "<RESULT>");
     }
 }
