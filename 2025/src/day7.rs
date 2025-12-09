7dfaf7f08e127c2ddd8cacbf11a1e709554fe53e
Chris Yeganeh <christianyeganeh@gmail.com>
2025-12-08 22:54:52 -0500

    Completed day 7 2025

--- 2025/src/day7.rs
+++ 2025/src/day7.rs
@@ -46,8 +46,30 @@ fn part1((start, input, height): &(usize, HashMap<usize, Vec<usize>>, usize)) ->
 }
 
 #[aoc(day7, part2)]
-fn part2((start, input, height): &(usize, HashMap<usize, Vec<usize>>, usize)) -> String {
-    todo!()
+fn part2((start, input, height): &(usize, HashMap<usize, Vec<usize>>, usize)) -> u64 {
+    let mut cols_to_check = HashMap::default();
+    cols_to_check.insert(*start, 1);
+    let mut r = 0;
+    while r < *height {
+        let row = input.get(&r);
+        if row.is_none() {
+            r += 1;
+            continue;
+        }
+        let row = row.unwrap();
+        let mut new_cols = cols_to_check.clone();
+        for col in cols_to_check.keys() {
+            if row.contains(col) {
+                let cur = *new_cols.get(&col).unwrap();
+                *new_cols.entry(col + 1).or_insert(0) += cur;
+                *new_cols.entry(col - 1).or_insert(0) += cur;
+                new_cols.remove(&col);
+            }
+        }
+        cols_to_check = new_cols;
+        r += 1;
+    }
+    cols_to_check.values().sum()
 }
 
 #[cfg(test)]
@@ -92,9 +114,23 @@ mod tests {
             3
         );
     }
+    #[test]
+    fn part2_example_2() {
+        assert_eq!(
+            part2(&parse(indoc! {"
+            ....S....
+            .........
+            ....^....
+            ...^.^...
+            .........
+            ........."
+            })),
+            4
+        );
+    }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE_INPUT)), "<RESULT>");
+        assert_eq!(part2(&parse(EXAMPLE_INPUT)), 40);
     }
 }
